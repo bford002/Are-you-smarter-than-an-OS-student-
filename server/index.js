2435ce6d033040da9418bd87b20d4f001866ba5c
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-05-20 17:44:48 -0500

    DB connection established through MongoAtlas, Server successfully running. Able to accept post/get requests

--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
+const { connection } = require('./db/index');
+
+//console.log(connection);
+
 const app = express();
-const PORT = 8080;
-
 const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
+const PORT = process.env.PORT || 8080;
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static(CLIENT_PATH));
 
+const usersRouter = require('./routes/users');
 
-app.listen(PORT, (err) => {
-  if (err) { console.log(err); }
-  console.log('Server listening on PORT', PORT);
-});
+app.use('/users', usersRouter);
+
+connection
+  .then(()=>{
+    console.log('MongoDB database connection established successfully');
+  })
+  .then(()=>{
+    app.listen(PORT, (err) => {
+      if (err) { console.log(err); }
+      console.log('Server listening on PORT', PORT);
+    });
+  })
+  .catch((err)=>{
+    console.error(err);
+  });
+
+
