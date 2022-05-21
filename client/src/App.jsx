01b67c8867066ca90079e96f6dd1c32a28d7ad4d
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-05-21 15:22:31 -0500

    testing static HTML page on non port 3000 url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
     });
   return (
     <div>
-      <h1>Welcome from AWS { new Date().toString() }</h1>
+      <h1>We made it! { new Date().toString() }</h1>
     </div>
   );
 };
