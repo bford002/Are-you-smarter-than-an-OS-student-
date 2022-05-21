f00e2915a00a0f819949c63d450133867b275e52
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-05-21 12:22:40 -0500

    axios installed, attempting to finalize deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 export const App = () => {
+  const persons = [];
+  axios.get('/users')
+    .then((users)=>{
+      console.log(users.data);
+    });
   return (
     <div>
-      <h1>Welcome from AWS{ new Date().toString() }</h1>
+      <h1>Welcome from AWS { new Date().toString() }</h1>
     </div>
   );
 };
