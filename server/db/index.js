9af58a36907945e6c25f5b7cffcdbe9df60b3519
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-05-23 16:55:00 -0500

    cleanup on .env variable stuff and button displaying on screen

--- server/db/index.js
+++ server/db/index.js
@@ -4,10 +4,6 @@ require('dotenv').config();
 
 const uri = process.env.ATLAS_URI;
 
-// mongoose.connect(uri, { useNewUrlParser: true });
-
-// const connection = mongoose.connection;
-
 const connection = mongoose.connect(uri, { useNewUrlParser: true });
 
 module.exports.connection = connection;
