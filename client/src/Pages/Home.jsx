c4e66ef18c0d5165afaf22e5b2987c553f145c4f
ibenyourbro <iben.oneal@gmail.com>
2022-05-24 12:16:27 -0500

    Navbar working with buttons

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const CLIENT_URL = process.env.CLIENT_URL;
-const PORT = process.env.PORT;
-
-const Home = ({user}) => {
+const Home = ({ user }) => {
   return (
     <div className='welcome'>
       {console.log}
-      <h1>Welcome, { user ? user.name : 'guest' }</h1>
+      <h1>Welcome, {user ? user.name : 'guest'}</h1>
 
-        { user ?
-         <a href={ `${CLIENT_URL}:${PORT}/auth/logout` }>
-            <button>Logout</button>
-         </a> 
-        : <a href={ `${CLIENT_URL}:${PORT}/auth/google` }>
-            <button>Login</button>
-          </a> }
-  </div>
-  )
-}
+      {user ? (
+        <a href={`${process.env.CLIENT_URL}:${process.env.PORT}/auth/logout`}>
+          <button>Logout</button>
+        </a>
+      ) : (
+        <a href={`${process.env.CLIENT_URL}:${process.env.PORT}/auth/google`}>
+          <button>Login</button>
+        </a>
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
