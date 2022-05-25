6d2c0bca8da44d61db06badeb8ba6c40c864a510
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-05-25 00:25:46 -0500

    Questions and answers populating on screen. Answers are buttons now, still unfunctional. Cleaned up characters in answers/question not printing properly (needs some work)

--- client/src/Components/Questions.jsx
+++ client/src/Components/Questions.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Question from './Question.jsx';
 
 
 
@@ -21,34 +22,7 @@ const Questions = () => {
     <div className='questions'>
       {
         questions ? questions.map((question, index, collection)=>{
-          const answers = [];
-          question.incorrect_answers.forEach((answer)=>{
-            answers.push(answer);
-          });
-          answers.push(question.correct_answer);
-          for (let i = 0; i < answers.length; i++) {
-            const rand = Math.floor(Math.random() * answers.length);
-
-            [answers[i], answers[rand]] = [answers[rand], answers[i]];
-          }
-          return (
-            <div className='QuestionAndAnswer' key={`question: ${index}`}>
-              <div className='question'>
-                {
-                  question.question
-                }
-              </div>
-              <div className='answers'>
-                
-                {
-                  answers.map((answer, index)=>{
-                    return <div className='answer' key={`answer${index}`} >{answer}</div>;
-                    
-                  })              
-                }
-              </div>
-            </div>
-          );
+          return <Question question = {question} key = {`q${index}`}/>;
         }) : 'Loading Questions...'
       }
     </div>
