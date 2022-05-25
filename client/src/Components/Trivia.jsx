a994dcf68a58528b73150d6361c8ea5bf4f9637a
ibenyourbro <iben.oneal@gmail.com>
2022-05-25 09:44:28 -0500

    User Profile and Routes and eslint error fix

--- client/src/Components/Trivia.jsx
+++ client/src/Components/Trivia.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import axios from 'axios';
 
 export const Trivia = () => {
-  axios.get('/users')
-    .then((users)=>{
-      console.log(users.data);
-    });
+  axios.get('/users').then((users) => {
+    console.log(users.data);
+  });
   return (
     <div>
-      <h1>We made it! { new Date().toString() }</h1>
+      <h1>We made it! {new Date().toString()}</h1>
     </div>
   );
 };
